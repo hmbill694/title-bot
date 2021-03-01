1662cd73f554d74b1591bf9d94894c48b8b792cf
Harrison Billings <harrisonbillings@Harrisons-MacBook-Pro.local>
2021-02-28 17:00:02 -0700

    title entry component completed

--- src/components/SeachHistory.vue
+++ src/components/SeachHistory.vue
@@ -1,14 +1,20 @@
 <template>
   <div>
     <b-list-group>
-      <b-list-group-item v-for="(item, index) in titles" :key="index">{{item.title}}</b-list-group-item>
+      <b-list-group-item v-for="(item, index) in titles" :key="index">
+        <TitleEntry :title="item.title" :url="item.url" />
+      </b-list-group-item>
     </b-list-group>
   </div>
 </template>
 
 <script>
 import { mapGetters } from 'vuex'
+import TitleEntry from '@/components/TitleEntry'
 export default {
+  components: {
+    TitleEntry
+  },
   computed: {
     ...mapGetters([
       'titles'
---
+++ src/components/TitleEntry.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="w-100 justify-between items-center">
+    <h5>{{title}}</h5>
+    <h6>{{url}}</h6>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'title-entry',
+  props: {
+    url: {
+      type: String,
+      default: ''
+    },
+    title: {
+      type: String,
+      default: ''
+    }
+  }
+}
+</script>
+
+<style>
+</style>
